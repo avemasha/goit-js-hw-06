393a59d7a48d0e1677245c405c97ff439436dc88
avemasha <masha.novosad@icloud.com>
2022-08-24 17:37:13 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const ulList = document.querySelector('#ingredients');
+
+// const foodIngredients = ingredients.forEach(ingredient  => {
+//   let items = document.createElement("li");
+//   items.classList.add('item');
+//   items.innerHTML = ingredient;
+//   ulList.append(items);
+//   parent.append(...items);
+// });
+
+// console.log(foodIngredients);
+
+const ulEl = document.querySelector('#ingredients');
+
+const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+
+
+ulEl.innerHTML = list;
