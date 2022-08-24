3a9e39e29727e6cd3cc439a93e91e2b7dfd2dd5a
avemasha <masha.novosad@icloud.com>
2022-08-24 15:13:38 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+const ulEl = document.querySelector("ul#categories");
+const countOfChildren = ulEl.children.length;
+
+console.log(`Number of categories: ${countOfChildren}`);
+ 
+const liEl = ulEl.children;
+
+
+
+
+
+for (let i = 0; i < liEl.length; i++) {
+ const childrenOfLi = liEl[i].children;
+   
+ const textContent = childrenOfLi[0].textContent;
+  console.log("Category: ", textContent);
+    const countOfChild = childrenOfLi[1].children.length;
+    console.log("Elements", countOfChild);
+ }
+
+
+
