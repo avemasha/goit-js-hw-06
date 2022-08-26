2b0fa3c3dfe9d7333fa19880b3c42fe2d976b020
avemasha <masha.novosad@icloud.com>
2022-08-26 18:34:04 +0300

    upd task 05

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+
+
+
+const input = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
+
+input.oninput = function () {
+    if (input.value === '') {
+       nameOutput.innerHTML = 'Anonymous';
+    } else{
+    nameOutput.innerHTML = input.value;
+    }
+}
