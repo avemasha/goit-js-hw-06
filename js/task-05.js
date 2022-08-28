5341749778632d4245a318cf5726a31f924622ee
avemasha <masha.novosad@icloud.com>
2022-08-28 19:41:44 +0200

    push

--- js/task-05.js
+++ js/task-05.js
@@ -7,11 +7,7 @@ const nameOutput = document.getElementById("name-output");
 input.addEventListener('input', onInputChange);
 
  function onInputChange(event) {
-    // if (input.value === '') {
-    //    nameOutput.innerHTML = 'Anonymous';
-    // } else{
-    // nameOutput.innerHTML = input.value;
-    // }
+
      console.log(event.currentTarget.value)
      nameOutput.textContent = event.currentTarget.value;
 }
