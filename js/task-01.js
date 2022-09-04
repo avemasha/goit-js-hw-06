d542e131069b4e8eaf7aa5336cc986673bdfda0b
avemasha <masha.novosad@icloud.com>
2022-09-04 20:39:52 +0200

    05

--- js/task-01.js
+++ js/task-01.js
@@ -5,18 +5,16 @@ console.log(`Number of categories: ${countOfChildren}`);
  
 const liEl = ulEl.children;
 
-
-
-
-
 for (let i = 0; i < liEl.length; i++) {
  const childrenOfLi = liEl[i].children;
    
- const textContent = childrenOfLi[0].textContent;
+ const textContent = childrenOfLi.firstElementChild.textContent;
   console.log("Category: ", textContent);
     const countOfChild = childrenOfLi[1].children.length;
     console.log("Elements", countOfChild);
  }
+
+
 
 
 
--- js/task-05.js
+++ js/task-05.js
@@ -8,7 +8,10 @@ input.addEventListener('input', onInputChange);
 
  function onInputChange(event) {
 
-     console.log(event.currentTarget.value)
-     nameOutput.textContent = event.currentTarget.value;
+       if (event.currentTarget.value === "") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = event.currentTarget.value;
+  }
 }
 
