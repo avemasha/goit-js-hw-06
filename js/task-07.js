a65402754dc647bbc0df961726d4f4f20381380c
avemasha <masha.novosad@icloud.com>
2022-09-04 20:45:16 +0200

    07

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,10 @@ const controlFont = document.getElementById("font-size-control");
 
 const text = document.getElementById("text");
 
-controlFont.oninput = function() {
-  text.style.fontSize = controlFont.value + "px";
-};
+// controlFont.oninput = function() {
+//   text.style.fontSize = controlFont.value + "px";
+// };
+
+controlFont.addEventListener("input", () => {
+  text.style.fontSize = `${controlFont.value}px`;
+});
